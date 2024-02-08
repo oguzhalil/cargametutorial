7cfa8569507a40d25a0fa93c89e516c33134628c
DESKTOP-KSLCI6E\oguz <oguzhan@beastburst.com>
2024-02-08 12:44:46 +0300

    Completed Car Tutorial Part 2

--- Assets/CarGameTutorial/Scripts/Car.cs
+++ Assets/CarGameTutorial/Scripts/Car.cs
@@ -10,11 +10,27 @@ public class Car : MonoBehaviour
 {
     public Axle[] axles;
     public float motorTorque = 1200f;
+    public float reverseMotorTorque = 300f;
     public float maxSteerAngle = 30f;
     public float brakeTorque = 5000f;
     public Transform COM;
     private Rigidbody rigidbody;
     public Transform prefabTyreVisual;
+    public float forwardSpeed = 0f;
+    public float backwardSpeed = 0f;
+    public float maxSpeed = 36f; // 36.0f * 3.6f = 130 kmh
+    public float maxReverseSpeed = 6f; // 6.0f  * 3.6f = 20 kmh
+
+    [Header("RPM Values")]
+    public float minRPM = 1000f;
+    public float maxRPM = 5000f;
+    public float currentRPM = 1000f;
+    public float rpmRevDuration = 3f;
+    public float rpmDampingRatio = 2f;
+    public AudioSource engineAudio;
+    public float minPitch = 0.5f;
+    public float maxPitch = 1.3f;
+    public AnimationCurve rpmCurve;
 
     // Start is called before the first frame update
     void Start()
@@ -39,14 +55,26 @@ public class Car : MonoBehaviour
         int handbrakeInput = Input.GetKey(KeyCode.Space) == true ? 1 : 0;
         float steerInput = Input.GetAxis("Horizontal");
 
+        forwardSpeed = Vector3.Dot(transform.forward, rigidbody.velocity);
+        backwardSpeed = Vector3.Dot(transform.forward * -1, rigidbody.velocity);
+
+        float currentTorque = GetRequiredTorque(forwardSpeed, maxSpeed, motorTorque);
+
+        if (forwardSpeed < 0.001f && brakeInput > 0)
+        {
+            throttleInput = -1;
+            brakeInput = 0;
+            currentTorque = GetRequiredTorque(backwardSpeed, maxReverseSpeed, reverseMotorTorque);
+        }
+
         for (int i = 0; i < axles.Length; i++)
         {
             var axle = axles[i];
 
             if (axle.applyTorque)
             {
-                axle.left.motorTorque = throttleInput * motorTorque;
-                axle.right.motorTorque = throttleInput * motorTorque;
+                axle.left.motorTorque = throttleInput * currentTorque;
+                axle.right.motorTorque = throttleInput * currentTorque;
             }
 
             if (axle.applySteering)
@@ -66,11 +94,37 @@ public class Car : MonoBehaviour
 
             UpdateWheelVisual(axle.left);
             UpdateWheelVisual(axle.right);
-
         }
+
+        // Simulate Engine RPM and Play Audio
+
+        float speedFactor = Mathf.InverseLerp(0, maxSpeed, forwardSpeed);
+        float newRPMDuration = rpmRevDuration + speedFactor * 3f;
+
+        // apply damping
+        float rpmDamping = throttleInput == 0 ? rpmDampingRatio : 0f;
+        currentRPM = Mathf.Lerp(currentRPM, minRPM, rpmDamping * Time.deltaTime);
+
+        currentRPM = Mathf.MoveTowards(currentRPM, maxRPM, (Time.deltaTime / newRPMDuration) * maxRPM * Mathf.Abs(throttleInput));
+
+        if(currentRPM >= maxRPM - 10 && speedFactor < 0.85f)
+            currentRPM = minRPM;
+
+        float rpmFactor = Mathf.InverseLerp(minRPM, maxRPM, currentRPM);
+        float curvedRPMFactor = rpmCurve.Evaluate(rpmFactor);
+        float targetPitch = Mathf.Lerp(minPitch, maxPitch, curvedRPMFactor);
+        engineAudio.pitch = Mathf.MoveTowards(engineAudio.pitch, targetPitch, (Time.deltaTime / newRPMDuration) * maxPitch);
     }
 
-    private void UpdateWheelVisual(WheelCollider wheelCollider) 
+    public float GetRequiredTorque(float speed, float maxSpeed, float maxMotorTorque)
+    {
+        float speedFactor = Mathf.InverseLerp(0f, maxSpeed, speed);
+        float requiredMotorTorque = Mathf.Lerp(maxMotorTorque, 0, speedFactor * speedFactor);
+
+        return requiredMotorTorque;
+    }
+
+    private void UpdateWheelVisual(WheelCollider wheelCollider)
     {
         Vector3 pos;
         Quaternion rot;
@@ -81,7 +135,7 @@ public class Car : MonoBehaviour
 
         Quaternion rotOffset = Quaternion.identity;
 
-        if(wheelCollider.transform.localPosition.x < 0) // sol teker
+        if (wheelCollider.transform.localPosition.x < 0) // sol teker
         {
             rotOffset = Quaternion.Euler(0f, 180f, 0f);
         }
